d2885b0dea3ab4d1b03083b5a43b12921430c047
Sergei <ffr@list.ru>
2021-09-26 19:35:38 +0300

    Homework Lesson2

---
+++ Lesson2/Lesson2_T3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определить, является ли введённое пользователем число чётным.
+
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Число {number} четное ");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} нечетное");
+            }
+            Console.ReadLine();
+        }
+    }
+}

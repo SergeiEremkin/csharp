15d30e55b736b52ca0f77eab1d29616be075cc6f
Sergei <ffr@list.ru>
2021-10-04 11:41:52 +0300

    Add Lesson 4

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+   
+    class Program
+    {
+        private static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return ($"{firstName} {lastName} {patronymic}");
+        }
+        static void Main(string[] args)
+        {
+            /*
+            Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+            принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. 
+            Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+            */
+            string human1 = GetFullName("Иванов", "Владимир", "Иванович");
+            string human2 = GetFullName("Тимощук", "Галина", "Васильевна");
+            string human3 = GetFullName("Григорян", "Валерий", "Иванович");
+            string human4 = GetFullName("Элтон", "Джон", " ");
+
+            Console.WriteLine($"{human1}\n{human2}\n{human3}\n{human4}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson4/Lesson4_T4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_T4
+{
+    class Program
+    {
+        private static int Fib(int n)
+        {
+            if (n <= 1) return n;
+
+            if (memF[n] != 0) return memF[n];
+
+            memF[n] = Fib(n - 2) + Fib(n - 1);
+            return memF[n];
+        }
+        private static int[] memF = new int[100];
+        static void Main(string[] args)
+        {
+
+            /*
+            (*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом. 
+ 
+            */
+            Console.WriteLine(Fib(55));
+            Console.ReadLine();
+
+       
+    }
+    }
+}
